6b50117aba93ce34e873618e592b5708747cda34
Nitish Maurya <nitishmaurya2255@gmail.com>
2025-10-02 02:00:36 +0530

    URL CHANGED

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,6 @@
 import { io } from "socket.io-client";
 
 // create socket connection
-const socket = io("http://localhost:8000/");
+const socket = io("https://connect-4-btvn.vercel.app/");
 
 export default socket;
