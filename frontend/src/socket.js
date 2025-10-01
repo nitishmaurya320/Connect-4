7b74fbd24436854cc3ac25c8168df392f16f428d
Nitish Maurya <nitishmaurya2255@gmail.com>
2025-10-02 02:08:25 +0530

    URL FIXED

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,6 @@
 import { io } from "socket.io-client";
 
 // create socket connection
-const socket = io("https://connect-4-btvn.vercel.app/");
+const socket = io("https://connect-4-syxe.vercel.app/");
 
 export default socket;
